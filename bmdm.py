2cc17621641785ba2de755591602012b2d507d73
M-Be-D <site.sabt.me@gmail.com>
2025-06-27 16:09:31 +0330

    Adding a history file along with a log function to record logs and methods activity.

--- bmdm.py
+++ bmdm.py
@@ -2,6 +2,7 @@
 import os
 import json
 import hashlib
+from datetime import datetime
 
 class BioMedDataManager:
     """
@@ -19,6 +20,7 @@ class BioMedDataManager:
         self.config_file = ".bmdm/config.json"
         self.objects_dir = ".bmdm/objects"
         self.history_dir = ".bmdm/history"
+        self.history_file = ".bmdm/history.log"
 
     def boot(self):
         """
@@ -28,6 +30,8 @@ class BioMedDataManager:
         # Checking the existence of the bmdm folder
         if os.path.isdir(self.bmdm_dir):
             print("The bmdm folder exists.")
+            self._log_activity("BOOT", "The bmdm folder exists.")
+
             if not os.path.isfile(self.index_file):
                 with open(self.index_file, "w") as index:
                     json.dump([], index)     
@@ -56,12 +60,15 @@ class BioMedDataManager:
                 json.dump({"manager": {"name": "", "email": ""}}, conf)
             with open(self.index_file, "w") as index:
                 json.dump([], index)
+                
+        self._log_activity("BOOT", "BMDM initialized")
     
     def config(self, name: str = None, email: str = None):
         """
         to configure and store user or doctor information
         """
         if not os.path.isdir(self.bmdm_dir):
+            self._log_activity("BOOT_ERROR", "Boot command not executed.")
             raise "First you need to load the boot. (python bmdm.py boot)"
 
         # To add name and email to config file
@@ -72,15 +79,19 @@ class BioMedDataManager:
             if email != None:
                 config["email"] = email
             conf.close()
+        
+        self._log_activity("CONFIG_UPDATE", f"Updated config: name={name}, email={email}")
     
     def admit(self, file_path: str):
         """
         To add medical data
         """
         if not os.path.isdir(self.bmdm_dir):
+            self._log_activity("BOOT_ERROR", "Boot command not executed.")
             raise "First you need to load the boot, run 'python bmdm.py boot' first"
         
         if not os.path.exists(file_path):
+            self._log_activity("PATH_ERROR", "Path does not exist.")
             raise "Path does not exist"
         
         def extract_metadata(file:str):
@@ -92,6 +103,7 @@ class BioMedDataManager:
                 file = os.path.basename(file)
                 parts = file.replace(".txt", "").split("_")
                 if len(parts) < 4:
+                    self._log_activity("TYPE_ERROR", "The input file type is incorrect.")
                     raise NameError("The file name is incorrect. the correct format is 'PATIENTID_STUDYDATE_MODALITY_DESCRIPTION.txt' ")
                 else:
                     metadata = {
@@ -118,6 +130,7 @@ class BioMedDataManager:
         # if input is file
         if os.path.isfile(file_path):
             if not file_path.endswith((".txt", ".json")):
+                self._log_activity("FORMAT_ERROR", "The format is invalid.")
                 raise TypeError("The format is invalid.")
             metadata, hash = extract_metadata(file_path)
             med_data = {hash[:8]: metadata}
@@ -125,6 +138,8 @@ class BioMedDataManager:
                 json.dump(med_data, index)
             with open(f"{self.objects_dir}/{hash}.data", 'w') as mdate:
                 json.dump(metadata, mdate)
+            
+            self._log_activity("ADMIT", "Information was recorded.")
         
         # if input is folder
         elif os.path.isdir(file_path):
@@ -140,13 +155,18 @@ class BioMedDataManager:
                     files.append(f)
                 
             if len(files) == 0:
+                self._log_activity("ADMIT_ERROR", "The specified folder does not contain a file with the correct format.")
                 print("The specified folder does not contain a file with the correct format.")
+
+            else:
+                self._log_activity("ADMIT", "Information was recorded.")
                 
     def stats(self):
         """
         to display a collection of data and statical information under management and observation
         """
         if not os.path.isdir(self.bmdm_dir):
+            self._log_activity("BOOT_ERROR", "Boot command not executed.")
             raise "First you need to load the boot, run 'python bmdm.py boot' first"
         
         try:
@@ -166,10 +186,14 @@ class BioMedDataManager:
                 # A series of statistical information to be added later
                 ...
 
+                self._log_activity("STATS", "All data was retrieved.")
+
         except Exception as e:
             if "No such file or directory" in e:
+                self._log_activity("BOOT_ERROR", "Boot command not executed.")
                 print("You must first run 'bmdm.py boot'.")
             else:
+                self._log_activity("STATS_ERROR", e)
                 print(f"ERROR: {e}")
     
     def tag(self, id_filename:str, key:str, value:str=None, remove= False):
@@ -177,6 +201,7 @@ class BioMedDataManager:
         to add or remove description tags for a specific data item
         """
         if not os.path.isdir(self.bmdm_dir):
+            self._log_activity("BOOT_ERROR", "Boot command not executed.")
             raise "First you need to load the boot, run 'python bmdm.py boot' first"
         
         with open(self.index_file, "a") as index:
@@ -186,13 +211,16 @@ class BioMedDataManager:
             id_s = [i["patient_id"] for i in dict(index_file.values())]
             name_s = [i["filename"] for i in dict(index_file.values())]
             if id_filename not in id_s or id_filename not in name_s:
+                self._log_activity("TAG_ERROR", f"The {id_filename} not found.")
                 raise "The entered ID does not exist."
             elif remove:
                 if key not in tags:
+                    self._log_activity("TAG_ERROR", "The entered key does not exist in tags list.")
                     raise "The entered key does not exist."
                 else:
                     index_file.values()["tags"].pop(key)
                     json.dump(index_file, index)
+                    self._log_activity("REMOVE_TAG", f"Tag with key {key} was removed from data {id_filename}.")
             else:
                 if id_filename in id_s:
                     index_file[hashs[id_s.index(id_filename)]]["tags"][key] = value
@@ -200,12 +228,14 @@ class BioMedDataManager:
                 elif id_filename in name_s:
                     index_file[hashs[name_s.index(id_filename)]]["tags"][key] = value
                     json.dump(index_file, index)
+                self._log_activity("ADD_TAG", f"The data {id_filename} was tagged with the value {key}={value}.")
     
     def find(self, filename=None, patient_id=None, study_date=None, modality=None, tag=None):
         """
         to search between data with a specific filter
         """
         if not os.path.isdir(self.bmdm_dir):
+            self._log_activity("BOOT_ERROR", "Boot command not executed.")
             raise "First you need to load the boot, run 'python bmdm.py boot' first"
         
         with open(self.index_file, "r") as index:
@@ -234,6 +264,17 @@ class BioMedDataManager:
                 
                 if match:
                     results.append(index_file[entry])
-            
+        self._log_activity("SEARCH", f"Searched with criteria: filename={filename}, patient_id={patient_id}, modality={modality}, date={study_date}, tag={tag}")
         return results
     
+    def _log_activity(self, activity_type, details):
+        """Log an activity to history file"""
+        with open(self.history_file, 'a') as f:
+            timestamp = datetime.now().isoformat()
+
+            with open(self.config_file, "r") as c_f:
+                config_file = json.load(c_f)
+                
+                user = str(config_file).replace('{', '').replace('}', '')
+            
+            f.write(f"{timestamp}|{activity_type}: {details}|{user}\n")
